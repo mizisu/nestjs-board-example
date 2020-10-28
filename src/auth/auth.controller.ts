cf481b2ae2182556e112cbb699dbca4ff8bacdeb
mizisu <alwltn960406@gmail.com>
2020-10-28 21:59:40 +0900

    <docs> update response model

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { AccessTokenDto } from './dto/accessToken.dto';
 import { LoginDto } from './dto/login.dto';
 import { SignInDto } from './dto/signIn.dto';
 
@@ -11,7 +12,11 @@ export class AuthController {
     @ApiOperation({
         operationId: 'Sign in',
     })
-    @ApiResponse({ status: 201, description: 'Sign in success' })
+    @ApiResponse({
+        status: 201,
+        description: 'Sign in success',
+        type: AccessTokenDto,
+    })
     @Post('/sign-in/')
     signIn(@Body() body: SignInDto) {
         this.authService.signIn(body);
@@ -20,7 +25,11 @@ export class AuthController {
     @ApiOperation({
         operationId: 'Login',
     })
-    @ApiResponse({ status: 201, description: 'Login success' })
+    @ApiResponse({
+        status: 201,
+        description: 'Login success',
+        type: AccessTokenDto,
+    })
     @Post('/login/')
     login(@Body() body: LoginDto) {
         return this.authService.login(body);
