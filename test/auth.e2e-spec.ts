627c750a601b1ec657e595d74fafd1e0a922a8db
mizisu <alwltn960406@gmail.com>
2020-11-15 22:08:29 +0900

    <test> added tests

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,5 @@
+import request from 'supertest';
+import { TestApp } from './app.test';
+
+describe('AuthController (e2e)', () => {
+});
--- test/boards.e2e-spec.ts
+++ test/boards.e2e-spec.ts
@@ -4,16 +4,17 @@ import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { TestApp } from './app.test';
 
-describe('AppController (e2e)', () => {
-    // it('/api/v1/boards/ (POST)', () => {
-    //     return request(TestApp.getHttpServer())
-    //         .get('api/v1/boards/')
-    //         .expect(200);
-    // });
+describe('BoardController (e2e)', () => {
+    it('/api/v1/boards/ (POST)', () => {
+        return request(TestApp.getHttpServer())
+            .post('api/v1/boards/')
+            .send({})
+            .expect(201);
+    });
 
     it('/api/v1/boards/ (GET)', () => {
         return request(TestApp.getHttpServer())
-            .get('api/v1/boards/')
+            .get('/api/v1/boards/')
             .expect(200);
     });
 });
