f35535e27f1668e9ff1228eaed1ec66020732f4c
mizisu <alwltn960406@gmail.com>
2020-11-22 20:20:17 +0900

    <test> added login test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { assert } from 'console';
 import request from 'supertest';
 import { TestApp } from './app.test';
 
@@ -11,5 +12,18 @@ describe('AuthController (e2e)', () => {
                 password: 'test1234',
             })
             .expect(201);
+    });
+
+    it('/api/v1/auth/login/ (POST)', () => {
+        return request(TestApp.getHttpServer())
+            .post('/api/v1/auth/login/')
+            .send({
+                username: 'testxcvvw',
+                password: 'test1234',
+            })
+            .expect(res => {
+                assert(res.status === 201);
+                assert(res.body.hasOwnProperty('access'));
+            });
     });
 });
